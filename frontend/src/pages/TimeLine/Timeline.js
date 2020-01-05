7166e2d13006b89c1f462a1cba39e8d79601ab9b
Márcio Alexandre <Freire Sindeaux>
2020-01-05 15:57:47 -0300

    feat: criação da pagina de tweets

--- frontend/src/pages/TimeLine/Timeline.js
+++ frontend/src/pages/TimeLine/Timeline.js
@@ -1,9 +1,42 @@
 import React, {Component} from 'react';
+import './Timeline.css';
+import twitterLogo from '../../twitter.svg'
+import axios from '../../services/api.js';
 
 export default class TimeLine extends Component {
+    state={
+        newTweet:''
+    }
+
+    handleInputChange = (event) => {
+        this.setState({
+            newTweet: event.target.value
+        })
+    }
+
+    handleNewTweet = async (event) => {
+        if(event.keyCode !== 13){
+            return;
+        }
+        const content = this.state.newTweet;
+        const author = localStorage.getItem("@GoTwitter:username");
+        await axios.post('tweets',{ author, content })
+
+        this.setState({newTweet: ''})
+    }
+
     render(){
         return(
-                <h1>TimeLine</h1>
+            <div className="timeline-wrapper">
+                <img src={twitterLogo} height={24}/> 
+                <form action="">
+                    <textarea 
+                        value={this.state.newTweet}
+                        onChange={this.handleInputChange}
+                        onKeyDown = {this.handleNewTweet}
+                        placeholder="O Que está acontecendo ?"/>
+                </form>
+            </div>
         );
     }
 }
